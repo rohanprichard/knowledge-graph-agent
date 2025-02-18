61f3bc26f356c7b5d21c797a80e62ba60a4f0bbf
rohanprichard <rohanprichard@gmail.com>
2025-02-19 02:27:48 +0530

    adding working graph

---
+++ conversational_kg_extraction_agent.py
@@ -0,0 +1,110 @@
+import os
+import json
+import rich
+import openai
+import uuid
+import instructor
+from pydantic import BaseModel
+from typing import List
+from dotenv import load_dotenv
+import requests
+from pyvis.network import Network
+
+
+load_dotenv()
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
+
+class Node(BaseModel):
+    id: str
+    label: str
+    entity_type: str
+
+
+class Edge(BaseModel):
+    id: str
+    source: Node
+    target: Node
+    label: str
+
+
+class Graph(BaseModel):
+    nodes: List[Node]
+    edges: List[Edge]
+
+
+class InferenceRequest(BaseModel):
+    text: str
+    graph: Graph
+
+
+class InferenceResponse(BaseModel):
+    response: str
+    new_nodes: List[Node]
+    new_edges: List[Edge]
+
+
+with open("assets/prompts/task-prompt.txt", "r") as file:
+    task_prompt = file.read()
+
+console = rich.console.Console()
+console.rule("[bold green]Task Prompt[/bold green]")
+console.print(task_prompt)
+console.rule()
+console.print("\n\n\n")
+
+print("Hi! What's your name?")
+name = input("You: ")
+
+user = Node(id=str(uuid.uuid4()), label=name, entity_type="person")
+knowledge_graph = Graph(nodes=[user], edges=[])
+
+
+client = instructor.from_openai(openai.OpenAI(api_key=DEEPSEEK_API_KEY, base_url="https://api.deepseek.com/v1"))
+
+def get_response(text: str):
+    response = client.completions.create(
+        model="deepseek-chat",
+        messages=[
+        {"role": "system", "content": task_prompt.format(graph=knowledge_graph)},
+        {"role": "user", "content": text},
+        ],
+        stream=False,
+        response_model=InferenceResponse
+        )
+    return response
+
+def visualize_graph(graph: Graph):
+    nodes = graph.nodes
+    edges = graph.edges
+
+    net = Network(height="750px", width="100%", font_color="#ffffff",  bgcolor="#222222", directed=False)
+
+    for node in nodes:
+        node_id = node.id
+        label = node.label
+        title = node.label
+        net.add_node(node_id, label=label, title=title)
+
+    for edge in edges:
+        source = edge.source.id
+        target = edge.target.id
+        label = edge.label
+        net.add_edge(source=source, to=target, label=label)
+
+    output_file = "knowledge_graph.html"
+    net.save_graph(output_file)
+
+
+
+def main_loop():
+    while True:
+        user_input = input("You: ")
+        if user_input == "/exit":
+            break
+        console.print(f"\n\n[bold blue]{user_input}[/bold blue] \n\n")
+        response = get_response(user_input)
+        print("Emma: ", response.response)
+        knowledge_graph.nodes.extend(response.new_nodes)
+        knowledge_graph.edges.extend(response.new_edges)
+
+main_loop()
